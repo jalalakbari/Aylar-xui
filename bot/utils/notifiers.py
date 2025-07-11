d21da834e5a62b2f90f7fbd98b4f8d8b85c4435d
jalalakbari <jalalakbari1374@gmail.com>
2025-07-11 04:59:14 +0330

    Update notifiers.py

--- bot/utils/notifiers.py
+++ bot/utils/notifiers.py
@@ -1,1 +1,8 @@
+from telegram import Bot
 
+def notify_owner(bot: Bot, owner_id: int, message: str):
+    bot.send_message(chat_id=owner_id, text=message)
+
+def notify_admins(bot: Bot, admin_ids: list, message: str):
+    for admin_id in admin_ids:
+        bot.send_message(chat_id=admin_id, text=message)
