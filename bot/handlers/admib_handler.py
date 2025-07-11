17d497631e18b1aba668dd9846bb418c4af1e9be
jalalakbari <jalalakbari1374@gmail.com>
2025-07-11 04:52:22 +0330

    Update admib_handler.py

--- bot/handlers/admib_handler.py
+++ bot/handlers/admib_handler.py
@@ -1,1 +1,7 @@
+from aiogram import types, Router from aiogram.filters import CommandStart
+
+admin_router = Router()
+
+@admin_router.message(CommandStart()) async def admin_start_handler(message: types.Message): await message.answer("پنل ادمین فعال شد. لطفاً یکی از گزینه‌ها را انتخاب کنید:") # اینجا می‌تونی دکمه‌های شیشه‌ای مربوط به ادمین رو اضافه کنی
+
 
